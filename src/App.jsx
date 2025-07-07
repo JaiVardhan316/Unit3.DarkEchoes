9db085a2b7bf19c0283c97a6f0864ffb6f79bcce
Jai Vardhan <pdmnvardhan510@gmail.com>
2025-07-06 21:16:00 -0500

    finished Dark Echoes workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,52 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an Episode to learn more.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>Episode {selectedEpisode.id}: {selectedEpisode.title}</h2>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  }
+
+  function Roster() {
+    return (
+      <section className="roster">
+        <h2 className="eps">Episodes</h2>
+        <ul className="roster">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1 className="title">Dark Echoes</h1>
+      </header>
+      <main>
+        <Roster />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
